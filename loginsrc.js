285712a1e0102ff26409cab10c6ab473aae962d3
dagi-ey <dagieyob114@gmail.com>
2021-04-24 21:56:59 +0300

    login source code added

---
+++ loginsrc.js
@@ -0,0 +1,30 @@
+let username = document.getElementById("Name");
+let billnumber = document.getElementById("BillNum");
+let phonenumber = document.getElementById("Phone");
+let emailAddress = document.getElementById("Email");
+
+let userR = document.getElementById("user");
+let empR = document.getElementById("employee");
+let admR = document.getElementById("admin");
+
+function getInfo(e){
+    e.preventDefault();
+    
+    if(username.value.length > 0 &&
+        billnumber.value.length > 0 &&
+        phonenumber.value.length > 0 &&
+        emailAddress.value.length > 0){
+            
+        }
+    
+    if(userR.checked){
+        window.location.href = "index.html";
+    }
+    if(empR.checked){
+        window.location.href = "employeepage.html";
+    }
+    if(admR.checked){
+        window.location.href = "adminpage.html";
+    }
+        
+}
