9b6878e4fb5e8c723bdcd69e592aabe12670b67d
DagimN <dagmnebiat@gmail.com>
2021-04-27 14:43:47 +0300

    missing files added

--- indexsrc.js
+++ indexsrc.js
@@ -1,8 +1,16 @@
 function getComplaint(){
     let complaints = JSON.parse(localStorage.getItem("complaint"));
+    let num = 0;
+    if(localStorage.getItem("notification") !== null){
+        num = parseInt(localStorage.getItem("notification"));
+    }
+    
     if(complaints === null){
         complaints = [];
     }
+
+    ++num;
     complaints.push(document.getElementById('complaint').value);
     localStorage.setItem('complaint', JSON.stringify(complaints));
+    localStorage.setItem("notification", num.toString());
 }
